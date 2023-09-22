6cf7e8977e12d74c2e1f92291fb9b6b64632c2e9
Hariharan <hariharan08112001@gmail.com>
2023-09-22 13:54:13 +0530

    code update

--- Swift_Learning/Closures.swift
+++ Swift_Learning/Closures.swift
@@ -1,8 +1,63 @@
-//
-//  Closures.swift
-//  Swift_Learning
-//
-//  Created by Hariharan R S on 21/09/23.
-//
+struct Workers {
+    var name: String
+    var location: String
+    var id: Int
+    var salary: Int
+}
 
-import Foundation
+struct Company {
+    var name: String
+    var workerId: Int
+}
+
+let listOfWorkers = [
+    Workers(name: "Aswin", location: "Madurai", id: 1, salary: 2000),
+    Workers(name: "Aravinth", location: "Chennai", id: 2, salary: 1000),
+    Workers(name: "Hariharan", location: "Madurai", id: 3, salary: 2000),
+    Workers(name: "Sivakumar", location: "Madurai", id: 4, salary: 1500),
+    Workers(name: "Ramachandran", location: "Tenkasi", id: 5, salary: 1200),
+    Workers(name: "Vignesh", location: "Chennai", id: 6, salary: 2000),
+]
+
+let workersListInCompany = [
+    Company(name: "Zoho", workerId: 1),
+    Company(name: "Google", workerId: 2),
+    Company(name: "Zoho", workerId: 3),
+    Company(name: "HCL", workerId: 4),
+    Company(name: "Zoho", workerId: 5),
+    Company(name: "ChainSys", workerId: 6),
+]
+
+// Function
+func sortBySalary(worker: Workers) -> String {
+    if(worker.salary > 1000) {
+        return worker.name
+    }
+    return ""
+}
+
+// Closure : let/var <variable_name>: (<parameter>) -> return_type = {}
+let nameOfWorker: (Workers) -> (Bool) = { worker in
+    return worker.salary >= 2000 && worker.location.elementsEqual("Madurai")
+}
+
+let nameOfWorkers: (Workers) -> Bool = { $0.salary >= 2000 && $0.location.elementsEqual("Madurai")}
+let getWorkersByCompany: (Company) -> Bool = { $0.name.elementsEqual("Zoho") }
+
+// In parameter we can pass only Objects
+let getNameById: (Workers, Int) -> Bool = { work, id in return id == work.id}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Swift_Learning/Enumerations.swift
+++ Swift_Learning/Enumerations.swift
@@ -1,8 +1,25 @@
-//
-//  Enumerations.swift
-//  Swift_Learning
-//
-//  Created by Hariharan R S on 21/09/23.
-//
+/*
+    1. Enum
+    2. Raw value
+    3. Associate value
+ */
 
-import Foundation
+enum socialMedia {
+    case INSTAGRAM
+    case WHATSAPP
+    case CLIQ
+    case LINKEDIN
+}
+
+func sendMessage(on platform: socialMedia) -> String {
+    switch platform {
+    case .INSTAGRAM:
+        return "Message is sending on Instagram"
+    case .WHATSAPP:
+        return "Message is sending on Whatsapp"
+    case .CLIQ:
+        return "Message is sending on Cliq"
+    case .LINKEDIN:
+        return "Message is sending on LinkedIn"
+    }
+}
